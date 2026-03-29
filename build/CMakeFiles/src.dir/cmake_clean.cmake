b124f0ded576925fb5acc4445dc767a1664378c9
unk-0 <alexflykt@hotmail.se>
2026-03-29 21:52:12 +0200

    cmake hello world

---
+++ build/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/link.d"
+  "CMakeFiles/src.dir/main.cpp.o"
+  "CMakeFiles/src.dir/main.cpp.o.d"
+  "src"
+  "src.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
